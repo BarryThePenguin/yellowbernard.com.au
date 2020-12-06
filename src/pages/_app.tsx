c834a06315a8cad35da6dc6d420331426651cb7b
Jonathan Haines <jonno.haines@gmail.com>
2020-12-06 21:53:34 +1100

    More things

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,46 +1,48 @@
-import React from 'react'
-import App, {AppProps} from 'next/app'
-import {TinaCMS, TinaProvider} from 'tinacms'
+import React from 'react';
+import App, {AppProps} from 'next/app';
+import {TinaCMS, TinaProvider} from 'tinacms';
 import {
 	GithubClient,
 	TinacmsGithubProvider,
-	GithubMediaStore,
-} from 'react-tinacms-github'
-import '../styles/style.css'
+	GithubMediaStore
+} from 'react-tinacms-github';
+import '../styles/style.css';
 
 type SiteProps = {
-	preview: boolean
-}
+	preview: boolean;
+};
 
 export default class Site extends App<AppProps<SiteProps>> {
-	cms: TinaCMS
+	cms: TinaCMS;
 
 	constructor(props: AppProps<SiteProps>) {
-		super(props)
+		super(props);
 
 		const github = new GithubClient({
 			proxy: '/api/proxy-github',
 			authCallbackRoute: '/api/create-github-access-token',
 			clientId: process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID,
 			baseRepoFullName: process.env.NEXT_PUBLIC_REPO_FULL_NAME,
-			baseBranch: process.env.NEXT_PUBLIC_BASE_BRANCH,
-		})
+			baseBranch: process.env.NEXT_PUBLIC_BASE_BRANCH
+		});
 
-		const enabled = true // Boolean(props.pageProps.preview)
+		const enabled = true; // Boolean(props.pageProps.preview);
+
+		console.log('pageProps', props);
 
 		this.cms = new TinaCMS({
 			enabled,
 			apis: {
-				github,
+				github
 			},
 			media: new GithubMediaStore(github),
 			sidebar: enabled,
-			toolbar: enabled,
-		})
+			toolbar: enabled
+		});
 	}
 
 	render() {
-		const {Component, pageProps} = this.props
+		const {Component, pageProps} = this.props;
 		return (
 			<TinaProvider cms={this.cms}>
 				<TinacmsGithubProvider
@@ -52,36 +54,36 @@ export default class Site extends App<AppProps<SiteProps>> {
 					<Component {...pageProps} />
 				</TinacmsGithubProvider>
 			</TinaProvider>
-		)
+		);
 	}
 }
 
 const onLogin = async () => {
-	const token = localStorage.getItem('tinacms-github-token') ?? null
-	const headers = new Headers()
+	const token = localStorage.getItem('tinacms-github-token') ?? null;
+	const headers = new Headers();
 
 	if (token) {
-		headers.append('Authorization', 'Bearer ' + token)
+		headers.append('Authorization', 'Bearer ' + token);
 	}
 
-	const resp = await fetch(`/api/preview`, {headers})
-	const data = await resp.json()
+	const resp = await fetch(`/api/preview`, {headers});
+	const data = await resp.json();
 
 	if (resp.status === 200) {
-		window.location.assign(window.location.pathname)
+		window.location.href = window.location.pathname;
 	} else {
-		throw new Error(data.message)
+		throw new Error(data.message);
 	}
-}
+};
 
 const onLogout = async () => {
 	return fetch(`/api/reset-preview`).then(() => {
-		window.location.reload()
-	})
-}
+		window.location.reload();
+	});
+};
 
 export interface EditLinkProps {
-	cms: TinaCMS
+	cms: TinaCMS;
 }
 
 export const EditLink = ({cms}: EditLinkProps) => {
@@ -89,5 +91,5 @@ export const EditLink = ({cms}: EditLinkProps) => {
 		<button type="button" onClick={() => cms.toggle()}>
 			{cms.enabled ? 'Exit Edit Mode' : 'Edit This Site'}
 		</button>
-	)
-}
+	);
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import {GetStaticProps} from 'next'
-import {usePlugin} from 'tinacms'
-import {getGithubPreviewProps, parseJson} from 'next-tinacms-github'
-import {useGithubJsonForm, useGithubToolbarPlugins} from 'react-tinacms-github'
-import {InlineForm, InlineTextarea, InlineText} from 'react-tinacms-inline'
-import Layout from '../components/layout'
+import React from 'react';
+import {GetStaticProps} from 'next';
+import {usePlugin} from 'tinacms';
+import styled from 'styled-components';
+import {getGithubPreviewProps, parseJson} from 'next-tinacms-github';
+import {useGithubJsonForm, useGithubToolbarPlugins} from 'react-tinacms-github';
+import {
+	InlineForm,
+	InlineTextarea,
+	InlineText,
+	InlineImageField
+} from 'react-tinacms-inline';
+import Layout from '../components/layout';
+
+const AboutUs = styled.div`
+	text-transform: uppercase;
+`;
 
 const formOptions = {
 	label: 'Home Page',
@@ -12,55 +22,121 @@ const formOptions = {
 		{
 			name: 'description',
 			component: 'textarea',
-			label: 'Description',
+			label: 'Description'
 		},
 		{
 			name: 'openingTimes',
 			component: 'text',
-			label: 'Opening Times',
+			label: 'Opening Times'
 		},
 		{
 			name: 'address',
 			component: 'text',
-			label: 'Address',
+			label: 'Address'
 		},
 		{
 			name: 'phone',
 			component: 'text',
-			label: 'Phone Number',
+			label: 'Phone Number'
 		},
-	],
-}
+		{
+			label: 'Map Image',
+			name: 'map',
+			component: 'image',
+			parse: (media) => `/static/${media.filename}`,
+			uploadDir: () => '/public/static/',
+			previewSrc: (fullSrc) => fullSrc.replace('/public', '')
+		}
+	]
+};
 
 type IndexProps = {
-	file: any
-}
+	file: any;
+};
 
 const Index = ({file}: IndexProps) => {
-	const [, form] = useGithubJsonForm(file, formOptions)
-	usePlugin(form)
-	useGithubToolbarPlugins()
+	const [, form] = useGithubJsonForm(file, formOptions);
+	usePlugin(form);
+	useGithubToolbarPlugins();
 
 	return (
 		<Layout>
 			<InlineForm form={form}>
 				<hr />
 
-				<InlineTextarea name="description" />
+				<p>
+					<InlineTextarea name="description" focusRing={false} />
+				</p>
 
 				<hr />
 
-				<InlineText name="openingTimes" />
+				<AboutUs>
+					<p>
+						<InlineText name="openingTimes" />
+					</p>
 
-				<InlineText name="address" />
+					<p>
+						<InlineText name="address" />
+					</p>
 
-				<InlineText name="phone" />
+					<p>
+						<InlineText name="phone" />
+					</p>
+
+					<p>
+						<a href="#" id="email-link">
+							EMAIL US
+						</a>
+					</p>
+
+					<form
+						method="post"
+						action="https://yellowbernard.com.au/index.php"
+						id="email-form"
+					>
+						<input type="hidden" name="send" value="1" />
+
+						<div>
+							<label>NAME</label>
+							<input type="text" name="name" id="name" />
+						</div>
+
+						<div>
+							<label>EMAIL</label>
+							<input type="text" name="email" id="email" />
+						</div>
+
+						<div>
+							<label>PHONE</label>
+							<input type="text" name="phone" id="phone" />
+						</div>
+
+						<div>
+							<label>MESSAGE</label>
+							<textarea name="message" id="message" />
+						</div>
+
+						<div>
+							<input
+								type="submit"
+								name="send-button"
+								id="send-button"
+								className="button"
+								value="SEND"
+							/>
+						</div>
+					</form>
+				</AboutUs>
+
+				<hr />
+
+				<InlineImageField name="map" />
 			</InlineForm>
 		</Layout>
-	)
-}
+	);
+};
 
-export default Index
+export default Index;
 
 export const getStaticProps: GetStaticProps<IndexProps> = async function ({
 	preview,
@@ -71,8 +147,8 @@ export const getStaticProps: GetStaticProps<IndexProps> = async function ({
 		return getGithubPreviewProps({
 			...previewData,
 			fileRelativePath: 'src/content/home.json',
-			parse: parseJson,
-		})
+			parse: parseJson
+		});
 	}
 
 	return {
@@ -82,8 +158,8 @@ export const getStaticProps: GetStaticProps<IndexProps> = async function ({
 			preview: false,
 			file: {
 				fileRelativePath: 'src/content/home.json',
-				data: (await import('../content/home.json')).default,
-			},
-		},
-	}
-}
+				data: (await import('../content/home.json')).default
+			}
+		}
+	};
+};
